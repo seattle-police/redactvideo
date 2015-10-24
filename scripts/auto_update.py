0aa0618bf7f6c1cfb1c1a5d04721c3c4c69fef23
Tim Clemans <timothy.clemans@seattle.gov>
2015-10-24 21:16:47 +0000

    add auto-update to rc.local

--- scripts/auto_update.py
+++ scripts/auto_update.py
@@ -1,3 +1,10 @@
+# cron is hard to set up programmatically so lets just run this file at boot
+# run git pull every five minutes 
+
 import os
-os.system('git stash save --keep-index')
-git_pull_request = os.popen('cd /home/ubuntu/redactvideodotorg; git pull --no-edit').read()
+while True:
+    os.system('cd /home/ubuntu/redactvideodotorg; git stash save --keep-index')
+    git_pull_request = os.popen('cd /home/ubuntu/redactvideodotorg; git pull --no-edit').read()
+    if not 'Already up-to-date.' in git_pull_request:
+        os.system('cd /home/ubuntu/redactvideodotorg; python auto_deploy.py &')
+    time.sleep(60*5)
