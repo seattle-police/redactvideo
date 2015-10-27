9e7f753559c808d72139d972ea9fb0c9979f97b7
Tim Clemans <timothy.clemans@seattle.gov>
2015-10-27 14:56:25 +0000

    make script for making safe public ami

--- scripts/make_safe_for_public_ami.sh
+++ scripts/make_safe_for_public_ami.sh
@@ -1,2 +1,3 @@
+# curl https://raw.githubusercontent.com/seattle-police/redactvideo/master/scripts/make_safe_for_public_ami.sh | sudo bash
 curl https://raw.githubusercontent.com/seattle-police/redactvideo/master/scripts/install.sh | sudo bash
 sudo shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
