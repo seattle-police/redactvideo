60d9b360ba3fe00522c4d6f39c661b71bab0fe0e
Tim Clemans <timothy.clemans@seattle.gov>
2015-10-28 07:01:09 +0000

    change install path to the py one

--- autodeploy.py
+++ autodeploy.py
@@ -3,7 +3,7 @@ import time
 # This script helps with auto deploy of new code by 
 # initializing database, installing new pip requirements,
 # and killing current instance of app.py
-os.system('sudo scripts/install.sh')
+os.system('sudo python scripts/install.py')
 os.system("sudo fuser -k 80/tcp")
 import datetime
 dt = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
