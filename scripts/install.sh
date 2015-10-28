2504dd8145cd3ff4953083e77a0cfcddbeb6f13e
Tim Clemans <timothy.clemans@seattle.gov>
2015-10-28 04:48:33 +0000

    add echo statements to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,13 @@
 # curl https://raw.githubusercontent.com/seattle-police/redactvideo/master/scripts/install.sh | sudo bash
+echo "Install Git"
 sudo apt-get -y install git
+echo "Remove RedactVideo"
 rm -rf redactvideo
+echo "Clone RedactVideo"
 git clone https://github.com/seattle-police/redactvideo.git
 sudo apt-get -y install python-pip
 sudo apt-get -y install python-dev libxml2-dev libxslt-dev
+echo "Install RethinkDB"
 . /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
 sudo apt-get update
