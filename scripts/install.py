0b14e40117dac0b141dbe9188eee3271ae7b90a6
Tim Clemans <timothy.clemans@seattle.gov>
2015-10-28 08:07:27 +0000

    fix bug causing dlib not to start

--- scripts/install.py
+++ scripts/install.py
@@ -25,6 +25,7 @@ print 'Install Flask'
 os.system('sudo pip install flask-socketio')
 print 'Install Dlib'
 os.system('sudo apt-get -y install libboost-python-dev cmake')
+os.system('cd /usr/lib/x86_64-linux-gnu; sudo ln -s libboost_python-py27.so.1.55.0 libboost_python.so.1.55.0')
 os.system('sudo cp redactvideo/binaries/dlib.so /usr/local/lib/python2.7/dist-packages/')
 print 'Install FFMPEG'
 os.system('wget http://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz')
