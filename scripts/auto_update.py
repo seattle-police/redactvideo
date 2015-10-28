6377b6ebd0c893caf784b8e36e348d1d80403c2e
Tim Clemans <timothy.clemans@seattle.gov>
2015-10-28 10:15:58 +0000

    every 5mins to 1 min

--- scripts/auto_update.py
+++ scripts/auto_update.py
@@ -1,5 +1,5 @@
 # cron is hard to set up programmatically so lets just run this file at boot
-# run git pull every five minutes 
+# run git pull every five minutes minute
 import time
 import os
 while True:
@@ -7,4 +7,4 @@ while True:
     git_pull_request = os.popen('cd ../; git pull --no-edit').read()
     if not 'Already up-to-date.' in git_pull_request:
         os.system('cd ../; python auto_deploy.py &')
-    time.sleep(60*5)
+    time.sleep(60)
