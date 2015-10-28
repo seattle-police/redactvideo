5cf537507532e68695a90614b1cfc60e7ac508e0
Tim Clemans <timothy.clemans@seattle.gov>
2015-10-28 19:14:42 +0000

    switch to python for make safe public ami script and remove rethinkdb data as well

---
+++ scripts/make_safe_for_public_ami.py
@@ -0,0 +1,4 @@
+# curl https://raw.githubusercontent.com/seattle-police/redactvideo/master/scripts/make_safe_for_public_ami.py | sudo bash
+curl https://raw.githubusercontent.com/seattle-police/redactvideo/master/scripts/install.py | sudo python
+os.system('sudo shred -u /etc/ssh/*_key /etc/ssh/*_key.pub')
+os.system('rm -rf rethinkdb_data')
