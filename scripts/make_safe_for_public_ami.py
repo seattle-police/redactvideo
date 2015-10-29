4853becb69bca99241f2553b38a14e683f1058b2
Tim Clemans <timothy.clemans@seattle.gov>
2015-10-29 02:23:40 +0000

    add removal of logs

--- scripts/make_safe_for_public_ami.py
+++ scripts/make_safe_for_public_ami.py
@@ -2,4 +2,4 @@
 import os
 os.system('curl https://raw.githubusercontent.com/seattle-police/redactvideo/master/scripts/install.py | sudo python')
 os.system('sudo shred -u /etc/ssh/*_key /etc/ssh/*_key.pub')
-os.system('rm -rf rethinkdb_data')
+os.system('rm -rf rethinkdb_data; rm -rf redactvideo_logs')
