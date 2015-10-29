e4362326eaba556741d7887657f25542034e3272
Tim Clemans <timothy.clemans@seattle.gov>
2015-10-29 02:06:00 +0000

    fix python script for making safe ami

--- scripts/make_safe_for_public_ami.py
+++ scripts/make_safe_for_public_ami.py
@@ -1,4 +1,5 @@
-# curl https://raw.githubusercontent.com/seattle-police/redactvideo/master/scripts/make_safe_for_public_ami.py | sudo bash
-curl https://raw.githubusercontent.com/seattle-police/redactvideo/master/scripts/install.py | sudo python
+# curl https://raw.githubusercontent.com/seattle-police/redactvideo/master/scripts/make_safe_for_public_ami.py | sudo python
+import os
+os.system('curl https://raw.githubusercontent.com/seattle-police/redactvideo/master/scripts/install.py | sudo python')
 os.system('sudo shred -u /etc/ssh/*_key /etc/ssh/*_key.pub')
 os.system('rm -rf rethinkdb_data')
